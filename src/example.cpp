e6b40b631372055f2ed7773e3d330c980a47b8c8
LukeDitria <Luke.Ditria@monash.edu>
2018-11-15 11:42:55 +1100

    updated example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -5,15 +5,17 @@
 #include <pcl/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-
 #include <pcl/sample_consensus/model_types.h>
-#include <pcl/sample_consensus/method_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/surface/convex_hull.h>
 #include <pcl/segmentation/extract_polygonal_prism_data.h>
-
+#include <pcl/features/normal_3d.h>
+#include <pcl/common/centroid.h>
+#include <pcl/features/crh.h>
+#include <visualization_msgs/MarkerArray.h>
+typedef pcl::Histogram<90> CRH90;
 ros::Publisher pub;
 
 void 
@@ -22,7 +24,6 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
   // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_inliers (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PCLPointCloud2 cloud_objects;
   pcl::fromROSMsg (*input, *cloud);
 
   pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
@@ -31,6 +32,8 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
   pcl::SACSegmentation<pcl::PointXYZ> seg;
   pcl::PointCloud<pcl::PointXYZ>::Ptr convexHull(new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PointCloud<pcl::PointXYZ>::Ptr objects(new pcl::PointCloud<pcl::PointXYZ>);
+
+  visualization_msgs::MarkerArray marker_array;
 
   // Optional
   seg.setOptimizeCoefficients (true);
@@ -79,11 +82,70 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 		extract.setIndices(objectIndices);
 		extract.filter(*objects);
 
-    pcl::toPCLPointCloud2(*objects, cloud_objects);
-    // Publish the model coefficients
-    sensor_msgs::PointCloud2 ros_cloud;
-    pcl_conversions::fromPCL(cloud_objects, ros_cloud);
-    pub.publish (ros_cloud);
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud (objects);
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance (0.02); // 2cm
+    ec.setMinClusterSize (100);
+    ec.setMaxClusterSize (25000);
+    ec.setSearchMethod (tree);
+    ec.setInputCloud (objects);
+    ec.extract (cluster_indices);
+
+    marker_array.markers.resize(cluster_indices.size());
+
+    int i = 0;
+      for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+      {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr object_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit){
+          object_cluster->points.push_back (objects->points[*pit]); //*
+        }
+        object_cluster->width = object_cluster->points.size ();
+        object_cluster->height = 1;
+        object_cluster->is_dense = true;
+
+        pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normalEstimation;
+        pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+        normalEstimation.setInputCloud(object_cluster);
+        normalEstimation.setRadiusSearch(0.03);
+        pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree(new pcl::search::KdTree<pcl::PointXYZ>);
+        normalEstimation.setSearchMethod(kdtree);
+        normalEstimation.compute(*normals);
+
+        // CRH estimation object.
+        pcl::CRHEstimation<pcl::PointXYZ, pcl::Normal, CRH90> crh;
+        crh.setInputCloud(object_cluster);
+        crh.setInputNormals(normals);
+        Eigen::Vector4f centroid;
+        pcl::compute3DCentroid(*object_cluster, centroid);
+        crh.setCentroid(centroid);
+
+        marker_array.markers[i].header.frame_id = "base_link";
+        marker_array.markers[i].header.stamp = ros::Time();
+        marker_array.markers[i].ns = "my_namespace";
+        marker_array.markers[i].id = i;
+        marker_array.markers[i].type = visualization_msgs::Marker::SPHERE;
+        marker_array.markers[i].action = visualization_msgs::Marker::ADD;
+        marker_array.markers[i].pose.position.x = centroid[0];
+        marker_array.markers[i].pose.position.y = centroid[1];
+        marker_array.markers[i].pose.position.z = centroid[2];
+        marker_array.markers[i].pose.orientation.x = 0.0;
+        marker_array.markers[i].pose.orientation.y = 0.0;
+        marker_array.markers[i].pose.orientation.z = 0.0;
+        marker_array.markers[i].pose.orientation.w = 1.0;
+        marker_array.markers[i].scale.x = 0.05;
+        marker_array.markers[i].scale.y = 0.05;
+        marker_array.markers[i].scale.z = 0.05;
+        marker_array.markers[i].color.a = 1.0;
+        marker_array.markers[i].color.r = 0.0;
+        marker_array.markers[i].color.g = 0.1;
+        marker_array.markers[i].color.b = 0.1;
+
+        i++;
+      }
+    pub.publish (marker_array);
   }
 }
 
@@ -98,7 +160,7 @@ main (int argc, char** argv)
   ros::Subscriber sub = nh.subscribe ("input", 1, cloud_cb);
 
   // Create a ROS publisher for the output model coefficients
-  pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+  pub = nh.advertise<visualization_msgs::MarkerArray> ("output", 1);
 
   // Spin
   ros::spin ();
